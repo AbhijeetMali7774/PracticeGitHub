cb8a2db60fe92bb90dc117cce95ba4e31bf8d704
AbhijeetMali7774 <106120451+AbhijeetMali7774@users.noreply.github.com>
2022-07-07 02:49:40 +0530

    Update YourPracticeClass.java

--- GitHubPracticeProject/src/yourPackage/YourPracticeClass.java
+++ GitHubPracticeProject/src/yourPackage/YourPracticeClass.java
@@ -5,6 +5,7 @@ public class YourPracticeClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello this is my Script of log in page");
+		System.out.println("Hello this is my Script of home page");
 	}
 
 }
